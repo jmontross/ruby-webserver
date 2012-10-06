4099f197581f1f1ac958da0f1fe7266b8ce1a9d1
Joshua Montross <joshua.montross@gmail.com>
2012-10-06 15:38:10 -0700

    push it up. 4 endpoints, some saving of state, viewing as json-- super simple... not so useful

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 # require 'socket'
 require 'yaml'
-
 require 'webrick' 
+require 'erb'
+require 'json'
+
 port = 8000
 # make it 80 if you want.  be sure to run as sudo.  
 
@@ -20,21 +22,13 @@ port = 8000
 #   session.close 
 # end
 
-routes = YAML.load_file('config/rest_endpoints.yml')
-
-server = WEBrick::HTTPServer.new(:Port => port)
-
-routes['endpoints'].each do |route,http_action| 
-  # register route to http_action
-  server.mount route, Kernel.constantize
-
-end
+ROUTES = YAML.load_file('config/rest_endpoints.yml')
 
 class Map < WEBrick::HTTPServlet::AbstractServlet
 
   # Process the request, return response
   def do_GET(request, response)
-    status, content_type, body = print_questions(request)
+    status, content_type, body = show_map(request)
 
     response.status = status
     response['Content-Type'] = content_type
@@ -42,23 +36,8 @@ class Map < WEBrick::HTTPServlet::AbstractServlet
   end
 
   # Construct the return HTML page
-  def print_questions(request)
-    html   = "<html><body><form method='POST' action='/save'>"
-    html += "Name: <input type='textbox' name='first_name' /><br /><br />";
-
-    # dbh = DBI.connect("DBI:Mysql:webarchive:localhost", "root", "pass")
-    # sth = dbh.execute("SELECT headline, story, id FROM yahoo_news where date >= '2004-12-01' and date <= '2005-01-01'")
-
-    #   # iterate over every returned news-story from the database
-    #   while row = sth.fetch_hash do
-    #       html += "<b>#{row['headline']}</b><br />\n"
-    #       html += "#{row['story']}<br />\n"
-    #       html += "<input type='textbox' name='#{row['id']}' /><br /><br />\n"
-    #  end
-    #  sth.finish
-
-    html += "<input type='submit'></form></body></html>"
-
+  def show_map(request)
+     html = File.read('public/index.html.erb')
     # Return OK (200), content-type: text/html, followed by the HTML itself
     return 200, "text/html", html
   end
@@ -67,34 +46,30 @@ end
 class Create < WEBrick::HTTPServlet::AbstractServlet
 
   # Process the request, return response
-  def do_GET(request, response)
-    status, content_type, body = print_questions(request)
+
+  def do_POST(request, response)
+    status, content_type, body = save_item(request)
 
     response.status = status
     response['Content-Type'] = content_type
     response.body = body
   end
 
-  # Construct the return HTML page
-  def print_questions(request)
-    html   = "<html><body><form method='POST' action='/save'>"
-    html += "Name: <input type='textbox' name='first_name' /><br /><br />";
+  # Save POST request into a text file
+  def save_item(request)
+	 if (filename = request.query['name'] )
+      f = File.open("tmp/save-#{filename}.#{Time.now.strftime('%H%M%S')}.yml", 'w')
 
-    # dbh = DBI.connect("DBI:Mysql:webarchive:localhost", "root", "pass")
-    # sth = dbh.execute("SELECT headline, story, id FROM yahoo_news where date >= '2004-12-01' and date <= '2005-01-01'")
-
-    #   # iterate over every returned news-story from the database
-    #   while row = sth.fetch_hash do
-    #       html += "<b>#{row['headline']}</b><br />\n"
-    #       html += "#{row['story']}<br />\n"
-    #       html += "<input type='textbox' name='#{row['id']}' /><br /><br />\n"
-    #  end
-    #  sth.finish
-
-    html += "<input type='submit'></form></body></html>"
-
-    # Return OK (200), content-type: text/html, followed by the HTML itself
-    return 200, "text/html", html
+      # Iterate over every POST'ed value and persist it to file
+      items = []
+      request.query.collect { | key, value | 
+      	  items << {key.to_sym => value}
+	      f.write("#{key}: #{value}\n") 
+	  }
+      f.close
+    end    
+    # Return OK Created (201), content-type: text/html, followed by the HTML itself
+    return 201, "text/html", {:status => "successfully saved items ", :items_save => items}.to_json
   end
 end
 
@@ -102,7 +77,7 @@ class View < WEBrick::HTTPServlet::AbstractServlet
 
   # Process the request, return response
   def do_GET(request, response)
-    status, content_type, body = print_questions(request)
+    status, content_type, body = show_items(request)
 
     response.status = status
     response['Content-Type'] = content_type
@@ -110,9 +85,28 @@ class View < WEBrick::HTTPServlet::AbstractServlet
   end
 
   # Construct the return HTML page
-  def print_questions(request)
-    html   = "<html><body><form method='POST' action='/save'>"
-    html += "Name: <input type='textbox' name='first_name' /><br /><br />";
+  def show_items(request)  	
+    starting_items = ROUTES['endpoints']['view']['viewables']
+    # ['endpoints']['view']['viewables']
+
+    # => [{:name=>"downtown_yoga_shala", :description=>"awesome yoga place - each wednesday six am"}] 
+    
+    html   = "<html><body><form method='POST' action='/create'>"
+    html  +=  "<h2> current items in our list </h2> <ul>"
+    starting_items.each do |item|
+      html += "<li> Name : #{item[:name]} </li>"
+    end
+     Dir.entries('tmp').each do |file| 
+	    if file.match(/\.yml/)
+	      y = YAML.load_file("tmp/#{file}")	
+	   	  html += "<li> Name : #{y['name']} </li>"   
+	    end
+	end  
+	html += "</ul>"
+
+    html += "Name: <input type='textbox' name='name' /><br /><br />";
+    html += "Description: <input type='textbox' name='description' /><br /><br />";
+    html += "Address: <input type='textbox' name='address' /><br /><br />";
 
     # dbh = DBI.connect("DBI:Mysql:webarchive:localhost", "root", "pass")
     # sth = dbh.execute("SELECT headline, story, id FROM yahoo_news where date >= '2004-12-01' and date <= '2005-01-01'")
@@ -130,4 +124,56 @@ class View < WEBrick::HTTPServlet::AbstractServlet
     # Return OK (200), content-type: text/html, followed by the HTML itself
     return 200, "text/html", html
   end
+
 end
+
+class Read < WEBrick::HTTPServlet::AbstractServlet
+
+  # Process the request, return response
+  def do_GET(request, response)
+    status, content_type, body = show_items(request)
+
+    response.status = status
+    response['Content-Type'] = content_type
+    response.body = body
+  end
+
+  # Construct the return HTML page
+  def show_items(request)  	
+
+    starting_items = ROUTES['endpoints']['view']['viewables']
+    # ['endpoints']['view']['viewables']
+
+    # => [{:name=>"downtown_yoga_shala", :description=>"awesome yoga place - each wednesday six am"}] 
+    
+    json = []
+    starting_items.each do |item|
+      json << item
+    end
+
+    Dir.entries('tmp').each do |file| 
+	    if file.match(/\.yml/)
+	      y = YAML.load_file("tmp/#{file}")	
+	      json << y
+	    end
+	end  
+    # Return OK (200), content-type: text/html, followed by the HTML itself
+    return 200, "text/json", json.to_json
+  end
+
+end
+
+## for images need to set DocumentRoot: "/Users/jmontross/github/jmontross/ruby_webserver" -- not working...
+server = WEBrick::HTTPServer.new(:Port => port)
+
+ROUTES['endpoints'].each do |route,configuration| 
+  # register route to http_action
+  puts "mounting /#{route}"
+  server.mount "/#{route}", Kernel.const_get(route.capitalize)  
+end
+
+## just to get somethign at the base
+server.mount '/', Map
+
+trap "INT" do server.shutdown end
+server.start
---
+++ test/test_app.rb
@@ -0,0 +1,15 @@
+require 'test/unit'
+# require '../app.rb'
+
+class TestTheApp < Test::Unit::TestCase
+
+def test_something
+  assert_equal(1,1)
+end
+
+def test_the_App
+   
+  assert_equal("I designed this well for testing... its a mix of functional and object oriented code", false) 
+end
+
+end
